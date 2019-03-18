d5731b0d932b69c193deaa897bf931e3f631f02d
raj-odc <ksraj.odc@gmail.com>
2019-03-18 13:22:02 +0530

    Add test cases for the class, modules

---
+++ spec/lib/utils_spec.rb
@@ -0,0 +1,35 @@
+require_relative '../../lib/utils'
+describe Utils do
+	context 'format the given date' do 
+		it 'should return as invalid when the given date is wrong format' do
+
+		end
+		
+		it 'should match the exact format as d.m.yyyy' do
+		end	
+	end
+
+	context 'format the given price as two digit' do 
+		it 'should return the same as given input if already formatted' do
+
+		end	
+		
+		it 'should match the exact format as two digit price' do
+			
+		end	
+	end
+
+	context 'calculate the drawdown' do
+		it 'should return the drawdown value for the given stock data' do
+		
+		end
+	end
+
+	context 'calculate_rate_of_return' do 
+		it 'should return the rate of return for the given date' do 
+
+		end
+	end
+
+
+end

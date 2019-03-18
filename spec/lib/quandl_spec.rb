3d8ccfe454080478ef481a5df63590e63151f25e
raj-odc <ksraj.odc@gmail.com>
2019-03-18 12:46:42 +0530

    Initiate rspec in the project   Add rspec as unit testing   Create require spec files   Describe some of the test cases

---
+++ spec/lib/quandl_spec.rb
@@ -0,0 +1,18 @@
+describe Quandl do
+
+  describe "get stock information" do
+
+    context 'get stock prices for given date range' do 
+    end
+
+    context 'get first 3 drawdowns for the stock data' do
+    end
+
+    context 'get the maximum drawdown' do 
+    end
+
+    context 'get the return for the given date range' do 
+    end  
+      
+  end
+end
